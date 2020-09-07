91332b8568e465e13b7ac8253d1ab9e02312b7ee
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-09-07 15:50:00 +0200

    Add quick_sort / merge_sort

---
+++ sequence01/merge_sort.py
@@ -0,0 +1,43 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
+def print_list(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+
+
+arr = [12, 11, 13, 5, 6, 7]
+print("Given array is", end="\n")
+print_list(arr)
+merge_sort(arr)
+print("Sorted array is: ", end="\n")
+print_list(arr)
