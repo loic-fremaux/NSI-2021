f0c56dfbc402ec44ab66f82ed0967f9981fef4e9
Loïc Frémaux <loic.fremaux1@gmail.com>
2020-12-08 09:39:30 +0100

    Fix

--- sequence06/exercices/ex15.py
+++ sequence06/exercices/ex15.py
@@ -11,16 +11,12 @@ P1 = Pile(6)\
 
 def renverser(P: Pile) -> Pile:
     R = Pile()
-    c = 0
     index = P
     while not P.est_vide():
         R.empiler(index.depiler())
-        c += 1
     return R
 
 
-c1 = 0
 index1 = renverser(P1)
 while not index1.est_vide():
     print(index1.depiler())
-    c1 += 1
